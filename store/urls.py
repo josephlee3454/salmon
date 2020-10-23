ea966c4dbf95608e4c7a3fa26d76a674d6ffaeb3
josephlee3454 <joseph.lee3454@gmail.com>
2020-10-22 21:00:32 -0700

    added login page

--- store/urls.py
+++ store/urls.py
@@ -3,7 +3,7 @@ from store.views import (
     store,
     cart,
     checkout,
-    login,
+    login_page,
     register
 
 )
@@ -12,7 +12,7 @@ urlpatterns = [
     
     path('',store, name = "store"),
     path('cart/',cart, name = "cart"),
-    path('checkout/',checkout, name = "checkout")
-    path('login/', login, name= 'login')
+    path('checkout/',checkout, name = "checkout"),
+    path('login/', login_page, name= 'login'),
     path('register/', register, name= 'register')
 ]
--- store/views.py
+++ store/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def store(request):
@@ -14,12 +16,28 @@ def checkout(request):
   context = {}
   return render(request, 'store/checkout.html', context)
 
-def login(request):
+def login_page(request):
+  if request.method == "POST":
+    username=request.POST.get('username')
+    password=request.POST.get('password')
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+      login(request, user)
+      return redirect('cart')
   context = {}
   return render(request, 'store/login.html', context)
+  #7cgx2JH!9fRFRfE
 
 def register(request):
-  context = {}
+  form = UserCreationForm()
+  if request.method == "POST":
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      form.save()
+      return redirect('login')
+
+  context = {'form':form}
+  
   return render(request, 'store/register.html', context)
 
 
